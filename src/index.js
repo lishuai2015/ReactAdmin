67012b7df705206d8c647c262378a8d3221c9a59
LiShuai <lishuai2015@163.com>
2020-09-28 11:41:45 +0800

    引入路由

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+/*
+    入口 js
+ */
+
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css'
 
+import App from "./App";
+
+/*
+将 App 组件渲染到 index 页面的 div 标签上
+*/
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App/>,
+    document.getElementById('root')
+)
