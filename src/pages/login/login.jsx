84666cb1bd32518d2174b167ea32ceeda01de841
LiShuai <lishuai2015@163.com>
2020-09-28 16:04:53 +0800

    form 表单统一验证

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -9,8 +9,63 @@ import './login.less';
 
 const Item = Form.Item; // 不能写在import之前
 
-export default class Login extends Component {
+class Login extends Component {
+
+    handleSubmit = (event) => {
+        // 阻止事件的默认行为
+        event.preventDefault();
+
+        // const form = this.props.form;
+        // const values = form.getFieldsValue();
+        // console.log('handleSubmit()', values)
+
+        // 对所有表单字段进行检验
+        this.props.form.validateFields((err, values) => {
+            console.log(err);
+            if (!err) {
+                console.log('提交登陆的ajax请求', values);
+                const {username, password} = values;
+            } else {
+                console.log('检验失败!')
+            }
+        });
+
+    }
+
+    /*
+        对密码进行自定义验证
+    */
+    /*
+        用户名/密码的的合法性要求
+            1). 必须输入
+            2). 必须大于等于4位
+            3). 必须小于等于12位
+            4). 必须是英文、数字或下划线组成
+    */
+    validatePwd = (rule, value, callback) => {
+        // console.log('validatePwd()', rule, value)
+        if(!value) {
+            callback('密码必须输入');
+        } else if (value.length < 4) {
+            callback('密码长度不能小于4位');
+        } else if (value.length > 12) {
+            callback('密码长度不能大于12位');
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            callback('密码必须是英文、数字或下划线组成');
+        } else {
+            callback(); // 验证通过
+        }
+        // callback('xxxx') // 验证失败, 并指定提示的文本
+    }
+
+
     render() {
+
+        // 得到具强大功能的 form 对象
+        const form = this.props.form;
+
+        const { getFieldDecorator } = form;
+
         return (
             <div className="login">
                 <header className='login-header'>
@@ -18,22 +73,84 @@ export default class Login extends Component {
                     <h1>React 项目: 后台管理系统</h1>
                 </header>
                 <section className='login-content'>
-                    <h3>用户登陆</h3>
-                    <Form onSubmit={this.login} className="login-form">
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
                         <Item>
-                            <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                   placeholder="用户名"/>
+
+                            {/*
+                            getFieldDecorator(id, options)
+                                id —— 配置对象
+                                options —— 选项
+                            */}
+
+                            {
+                                getFieldDecorator('username', { // 配置对象: 属性名是特定的一些名称
+                                    // 声明式验证: 直接使用别人定义好的验证规则进行验证
+                                    rules: [
+                                        { required: true, whitespace: true, message: '用户名必须输入' },
+                                        { min: 4, message: '用户名至少4位' },
+                                        { max: 12, message: '用户名最多12位' },
+                                        { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成' },
+                                    ],
+                                    initialValue: 'admin', // 初始值
+                                })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名"/>)
+                            }
                         </Item>
-                        <Item>
-                            <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                   type="password" placeholder="密码"/>
-                        </Item>
-                        <Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
-                        </Item>
+                        <Form.Item>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        {
+                                            validator: this.validatePwd
+                                        }
+                                    ]
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="密码"
+                                    />
+                                )
+                            }
+                        </Form.Item>
+                        <Item><Button type="primary" htmlType="submit" className="login-form-button">登录</Button></Item>
                     </Form>
                 </section>
             </div>
         );
     }
 }
+
+
+
+/*
+知识点：
+1. 高阶函数
+    1). 一类特别的函数
+        什么情况是高阶函数？ 1）接受函数类型的参数； 2）返回值是函数
+    2). 常见
+        a. 定时器: setTimeout()/setInterval()
+        b. Promise: Promise(() => {}) then(value => {}, reason => {})
+        c. 数组遍历相关的方法: forEach()/filter()/map()/reduce()/find()/findIndex()
+        d. 函数对象的bind()
+        e. Form.create()() / getFieldDecorator()()
+    3). 高阶函数更新动态, 更加具有扩展性
+
+2. 高阶组件
+    1). 本质就是一个函数
+    2). 接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+    3). 作用: 扩展组件的功能
+    4). 高阶组件也是高阶函数: 接收一个组件函数, 返回是一个新的组件函数
+
+ */
+/*
+    包装 Form 组件生成一个新的组件: Form(Login)
+    新组件会向 Form 组件传递一个强大的对象属性: form
+ */
+const WrapLogin = Form.create()(Login);
+export default WrapLogin;
+
+/*
+    1. 前台表单验证
+    2. 收集表单输入数据
+ */
