3793402abd9192411b1e9b26423ad92cc3535f99
JackLi <lishuai2015@163.com>
2020-09-30 00:19:06 +0800

    自动打开当前菜单的子列表并选中菜单项

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -4,7 +4,6 @@ import {Icon, Menu} from 'antd';
 
 import logo from '../../assets/images/logo.png'
 import './index.less'
-import memoryUtils from "../../utils/memoryUtils";
 
 import menuList from '../../config/menuConfig'
 
@@ -15,8 +14,8 @@ const SubMenu = Menu.SubMenu;
  */
 class LeftNav extends Component {
 
-    // 根据menu的数据数组生成对应的标签数组
-    getMenuNodes = (menuList) => {
+    // 方式一   根据menu的数据数组生成对应的标签数组：使用map() + 递归调用
+    getMenuNodes_map = (menuList) => {
         return menuList.map(item => {
             /*
             {
@@ -74,7 +73,72 @@ class LeftNav extends Component {
         })
     }
 
+    // 方式二   根据menu的数据数组生成对应的标签数组：使用reduce() + 递归调用
+    getMenuNodes = (menuList) => {
+
+        // 得到当前请求的路由路径
+        const path = this.props.location.pathname
+        console.log(path)
+
+        return menuList.reduce((pre, item) => {
+            console.log(pre)
+            console.log(item)
+
+            // 向pre添加<Menu.Item>
+            if (!item.children) {
+                pre.push((
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon}/>
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                ))
+            } else {
+
+                /* 展开选中的二级菜单 */
+                // 查找一个与当前请求路径匹配的子Item
+                const cItem = item.children.find(cItem => path.indexOf(cItem.key)===0)
+                // 如果存在, 说明当前item的子列表需要打开
+                if (cItem) {
+                    this.openKey = item.key
+                }
+
+                // 向pre添加<SubMenu>
+                pre.push((
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                <Icon type={item.icon}/>
+                                <span>{item.title}</span>
+                            </span>
+                        }
+                    >
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu>
+                ))
+            }
+
+            return pre
+        }, [])
+    }
+
+    /*
+    在第一次render()之前执行一次
+    作用：为第一个render()准备数据(必须同步的)
+   */
+    componentWillMount () {
+        this.menuNodes = this.getMenuNodes(menuList)
+    }
+
     render() {
+
+        // 得到当前请求的路由路径
+        let path = this.props.location.pathname
+
+        // 得到需要打开菜单项的key
+        const openKey = this.openKey
 
         return (
             <div className="left-nav">
@@ -86,35 +150,12 @@ class LeftNav extends Component {
                 <Menu
                     mode="inline"
                     theme="dark"
+                    selectedKeys={[path]}
+                    defaultOpenKeys={[openKey]}
                 >
-                    {/*
 
-
-                    <Menu.Item key="1">
-                        <Link to='/home'>
-                            <Icon type="pie-chart"/>
-                            <span>首页</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <SubMenu
-                        key="sub1"
-                        title={<span>
-                            <Icon type="mail"/>
-                            <span>商品</span>
-                        </span>}>
-                        <Menu.Item key="5">
-                            <Icon type="mail"/>
-                            <span>品类管理</span>
-                        </Menu.Item>
-                        <Menu.Item key="6">
-                            <Icon type="mail"/>
-                            <span>商品管理</span>
-                        </Menu.Item>
-                    </SubMenu>
-                    */}
                     {
-                        this.getMenuNodes(menuList)
+                        this.menuNodes
                     }
 
                 </Menu>
@@ -125,7 +166,7 @@ class LeftNav extends Component {
 
 /*
 withRouter高阶组件:
-包装非路由组件, 返回一个新的组件
-新的组件向非路由组件传递3个属性: history/location/match
+    包装非路由组件, 返回一个新的组件
+    新的组件向非路由组件传递3个属性: history/location/match
  */
 export default withRouter(LeftNav)
